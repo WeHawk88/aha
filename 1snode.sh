8fd0187ec68ea38b0d08c0dc57277b3acc22d18f
WeHawk88 <149553644+WeHawk88@users.noreply.github.com>
2023-11-01 20:07:37 +0800

    Update 1snode.sh

--- 1snode.sh
+++ 1snode.sh
@@ -3,5 +3,6 @@
 #This fetches block IDs and corresponding merkleroots produced by the network. 
 
 cd
-cd XENGPUMiner
+cd
+cd xenminer
 python3 syncnode.py Eth_Address		#Change ETH_Address with your metamask wallet address

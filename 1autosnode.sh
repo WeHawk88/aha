0d4f641b077a434cfbc4cae1e78287168359b633
WeHawk88 <149553644+WeHawk88@users.noreply.github.com>
2023-11-01 20:07:17 +0800

    Update 1autosnode.sh

--- 1autosnode.sh
+++ 1autosnode.sh
@@ -2,7 +2,8 @@
 #every 5 minutes (300 s) , you can change 5 mins to your desire time
 
 cd
-cd XENGPUMiner
+cd
+cd xenminer
 while sleep 300; 
 do python3 syncnode.py ETH_Address;    #Change ETH_Address with your metamask wallet address
 done	

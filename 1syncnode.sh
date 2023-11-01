dab3fd251459a9c210c24cf2019fe81f7eec665a
WeHawk88 <149553644+WeHawk88@users.noreply.github.com>
2023-11-01 12:34:38 +0800

    Delete 1syncnode.sh

--- 1syncnode.sh
+++
@@ -1,6 +0,0 @@
-#Run syncnode (remember to edit your eth add in nano )
-#Syncnode is for full archival data sync.
-#This fetches block IDs and corresponding merkleroots produced by the network. 
-
-cd XENGPUMiner/utils
-python3 syncnode.py ETH_Address		#Change ETH_Address with your metamask wallet address
